66db6049cca1b79f05648054b454c9f282401ac2
rjmasache <royerjmasache@gmail.com>
2024-10-16 10:03:20 -0500

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const poppins = Poppins({
+    weight: ["400"],
+    subsets: ["latin"],
+});
